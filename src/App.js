9052f2e5090fd073af5a2acd86555742b0a3c4e2
jjj-u-yeon <jueyonhong0822@gmail.com>
2023-02-04 00:47:32 -0500

    add css

--- src/App.js
+++ src/App.js
@@ -37,44 +37,43 @@ function App() {
       </header>
         {JSON.stringify(datas) !== '{}'
             ?(
-                <div>
-                    <div><span>LAST UPDATED: </span>{datas.time.updated}</div>
-                <Table striped bordered hover>
+                <div className="innerDiv">
+                    <Table>
                     <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>EUR</th>
-                        <th>GBP</th>
-                        <th>USD</th>
-                    </tr>
+                        <tr>
+                            <th></th>
+                            <th>EUR</th>
+                            <th>GBP</th>
+                            <th>USD</th>
+                        </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <td>code</td>
+                        <td className="fields">code</td>
                         <td>{datas.bpi.EUR.code}</td>
                         <td>{datas.bpi.GBP.code}</td>
                         <td>{datas.bpi.USD.code}</td>
                     </tr>
                     <tr>
-                        <td>Symbol</td>
+                        <td className="fields">Symbol</td>
                         <td>{datas.bpi.EUR.symbol}</td>
                         <td>{datas.bpi.GBP.symbol}</td>
                         <td>{datas.bpi.USD.symbol}</td>
                     </tr>
                     <tr>
-                        <td>Rate</td>
+                        <td className="fields">Rate</td>
                         <td>{datas.bpi.EUR.rate}</td>
                         <td>{datas.bpi.GBP.rate}</td>
                         <td>{datas.bpi.USD.rate}</td>
                     </tr>
                     <tr>
-                        <td>Rate Float</td>
+                        <td className="fields">Rate (Float)</td>
                         <td>{datas.bpi.EUR.rate_float}</td>
                         <td>{datas.bpi.GBP.rate_float}</td>
                         <td>{datas.bpi.USD.rate_float}</td>
                     </tr>
                     <tr>
-                        <td>Description</td>
+                        <td className="fields">Description</td>
                         <td>{datas.bpi.EUR.description}</td>
                         <td>{datas.bpi.GBP.description}</td>
                         <td>{datas.bpi.USD.description}</td>
@@ -87,7 +86,9 @@ function App() {
                     <p>no datas</p>
                 </div>
         )}
+        <div className="innerDiv update"><span><b>Last Updated: </b></span>{datas.time.updated}
             <button className="updateButton" onClick={() => updateInfo()}> update </button>
+        </div>
     </div>
     );
 }
